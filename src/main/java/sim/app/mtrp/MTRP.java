f18204984bb480a14af6303b2193cf7785c7c175
dwicke <drewwicke@gmail.com>
2017-02-16 16:11:05 -0500

    Added in the Mason dependency to the pom and created a start point for the project MTRP

---
+++ src/main/java/sim/app/mtrp/MTRP.java
@@ -0,0 +1,30 @@
+package sim.app.mtrp;
+
+import sim.engine.*;
+
+/**
+ * Entry into the mason simulator.
+ * Created by drew on 2/16/17.
+ */
+public class MTRP extends SimState {
+
+    private static final long serialVersionUID = 1;
+    private static String[] myArgs;
+
+    public MTRP(long seed) {
+        super(seed);
+    }
+
+    public static void main(String[] args) {
+        myArgs = args;
+        doLoop(MTRP.class, args);
+        System.exit(0);
+    }
+
+
+    @Override
+    public void start() {
+        super.start();
+        // here we go!
+    }
+}
