54949506a354573e512b36747260de3d0f2b5500
dwicke <drewwicke@gmail.com>
2017-02-20 21:39:30 -0500

    Created the base class shells and populated the fields in mtrp

---
+++ src/main/java/sim/app/mtrp/main/Bondsman.java
@@ -0,0 +1,7 @@
+package sim.app.mtrp.main;
+
+/**
+ * Created by drew on 2/20/17.
+ */
+public class Bondsman {
+}
--- src/main/java/sim/app/mtrp/main/MTRP.java
+++ src/main/java/sim/app/mtrp/main/MTRP.java
@@ -2,6 +2,7 @@ package sim.app.mtrp.main;
 
 import sim.engine.*;
 
+
 /**
  * Entry into the mason simulator.
  * Created by drew on 2/16/17.
@@ -10,6 +11,32 @@ public class MTRP extends SimState {
 
     private static final long serialVersionUID = 1;
     private static String[] myArgs;
+
+
+    public int numAgents = 4;
+    public int maxCarrySize = 4;
+    public double startFunds = 100;
+    public double fuelCapacity = 1000;
+
+    public int numNeighborhoods = 4;
+    public int numDepos = 2;
+    public int depoCapacity = 20; // how many resources of each type can the depo carry
+    public int depoRefreshRate = 100; // every one hundred timesteps we refresh the resources in the depo... this also is very arbitrary and could be investigated
+
+    public int numResourceTypes = 3; // fuel is not a resource included here.
+    public double maxCostPerResource = 20.0; // for each type of resource we get the price and set it for all of the depos
+
+    public double fuelCost = 1.0;
+    public int maxNumTasksPerNeighborhood = 6;
+    public int maxJobLength = 10; // change...
+    public int maxNumResourcesPerJob = numResourceTypes*2;
+    public double basebounty = 100;
+
+
+    public int simWidth = 100;
+    public int simHeight = 100;
+
+
 
     public MTRP(long seed) {
         super(seed);
@@ -26,5 +53,12 @@ public class MTRP extends SimState {
     public void start() {
         super.start();
         // here we go!
+
+
+        // First create the neighborhoods.  Use the mean location as the location for the depos
+
+
+
+
     }
 }
